2c69dfa13c65fe0367ba12bc86761c7c9fa573df
Issa Itani <>
2025-11-13 11:31:56 +0200

    added database configuration and connected to database

---
+++ back/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+header ("Access-Control-Allow-Origin: *");
+header ("Access-Control-Allow-Headers: Content-Type");
+header ("Content-Type: application/json; charset=utf-8");
+
+include "config.php";
+
+$conn = new mysqli($db_config["host"], $db_config["user"], $db_config["password"], $db_config["db"]);
+
+if($conn -> connect_error){
+    die(json_encode(["Error" => "Connection failed:" . $conn -> connect_error]));
+}
+
+$conn ->set_charset("utf8");
+
+?>
