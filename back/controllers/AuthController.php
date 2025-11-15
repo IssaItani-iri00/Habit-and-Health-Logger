dc296dae62bb6fec8655927d66cd734267be2e2d
Issa Itani <>
2025-11-15 19:44:29 +0200

    removed middleware to use local storage, and added login validation

--- back/controllers/AuthController.php
+++ back/controllers/AuthController.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . "/../helpers/AuthMiddleware.php";
 require_once __DIR__ . "/../helpers/Response.php";
 require_once __DIR__ . "/../helpers/Validator.php";
 require_once __DIR__ . "/../models/User.php";
@@ -14,7 +13,7 @@ class AuthController{
             return Response::error("Email is required", 400);
         }
 
-        if(!Validator::email($data)){
+        if(!Validator::email($data["email"])){
             return Response::error("Invalid email format", 400);
         }
 
@@ -26,9 +25,9 @@ class AuthController{
             return Response::error("Password cant be less than 8 characters", 400);
         }
 
-        $existingUser = User::findByEmail($data);
+        $existingUser = User::findByEmail($data["email"]);
         if($existingUser){
-            return Response::error("Email is already registered", 400);
+            return Response::error("Email is already registered", 409);
         }
 
         $hashedPassword = password_hash($data["password"], PASSWORD_DEFAULT);
@@ -37,9 +36,41 @@ class AuthController{
 
         return Response::success("User registered successfully!", ["user_id" => $userId], 201);
     }
+
+    public static function login($data){
+
+        if(!Validator::required($data["email"] ?? null)){
+            return Response::error("Email is required", 400);
+        }
+
+        if(!Validator::email($data)){
+            return Response::error("Invalid email format", 400);
+        }
+
+        if(!Validator::required($data["password"] ?? null)){
+            return Response::error("Password cant be empty", 400);
+        }
+
+        $user = User::findByEmail($data["email"]);
+        if(!$user){
+            return Response::error("Email or password are invalid", 401);
+        }
+
+        if(!password_verify($data["password"], $user["password_hash"])){
+            return Response::error("Email or password are invalid", 401);
+        }
+
+        return Response::success("LogIn Successful", [
+            "user_id" => $user["id"],
+            "user_name" => $user["name"],
+            "user_email" => $user["email"],
+            "user_role" => $user["role"]
+        ]);
+    }
+
+    public static function logout(){
+        return Response::success("Logged out successfully");
+    }
 }
-
-
-
 
 ?>
