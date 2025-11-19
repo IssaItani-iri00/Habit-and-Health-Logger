3abdc917d759bfa360d274ea2c0e80589177bced
Issa Itani <>
2025-11-19 23:05:32 +0200

    admin username and password for testing here

--- back/db-configuration/config-example.php
+++ back/db-configuration/config-example.php
@@ -1,5 +1,5 @@
 <?php
-// Hi again Charbel :), please enter your password below(if you have any), and change the include in the db_connect file
+// Hi again Charbel :), please enter your password below(if you have any), and change the include in the db_connect file ######################## USE charbeldaoud@gmail.com AND charbeldaoud123 TO ACCESS ADMIN PAGE WHEN TESTING
 $db_config = [
     "host" => "localhost",
     "user" => "root",
